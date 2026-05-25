0fb30d68f527319aeae07688be7a086986d25e84
Kavin-Kaarthik <sikavin137@gmail.com>
2026-05-25 12:05:35 +0530

    Update page.tsx

--- happilivhostels/src/app/boys-hostel-in-kengeri/page.tsx
+++ happilivhostels/src/app/boys-hostel-in-kengeri/page.tsx
@@ -298,6 +298,7 @@ export default function BoysHostelKengeri() {
               <div className="bg-[#FFFFFF2A] p-4 rounded-xl">
                 <p className="font-semibold mb-1">Email</p>
                 <p>happilivhostels@gmail.com</p>
+                <p>vibashappilivhostel@gmail.com</p>
               </div>
 
             </div>
